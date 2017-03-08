6e65e13efbd65d8441eefa80740fd7d75189bcf1
Marcelo Sampaio <marcelo@artlogica.com.br>
2017-03-08 13:42:50 -0300

    added keychain engine

--- Keychain/ViewController.h
+++ Keychain/ViewController.h
@@ -8,7 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
+#import <Security/Security.h>
+
+
 @interface ViewController : UIViewController
+
+
+
 
 
 @end
--- Keychain/ViewController.m
+++ Keychain/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "FDKeychain.h"
+
+#define KEYCHAIN_KEY        @"StoredKeychainData"
+#define KEYCHAIN_SERVICE    @"KeychainService"
 
 @interface ViewController ()
 
@@ -14,16 +18,60 @@
 
 @implementation ViewController
 
+
+#pragma mark - View Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UI Actions
+- (IBAction)storeKeyChain:(id)sender {
+    // store some value in keychain
+
+    NSString *keychainData = @"UUID of the device";
+    NSError *error = nil;
+    
+    [FDKeychain saveItem: keychainData
+                  forKey: KEYCHAIN_KEY
+              forService: KEYCHAIN_SERVICE
+                   error: &error];
+    
+    NSLog(@"*** KEYCHAIN has been store");
+    
 }
+
+- (IBAction)getKeyChain:(id)sender {
+    
+    NSError *error = nil;
+    
+    NSString *keychainData = [FDKeychain itemForKey: KEYCHAIN_KEY
+                                     forService: KEYCHAIN_SERVICE
+                                          error: &error];
+    
+    NSLog(@"** keychain data retrieved: %@",keychainData);
+    
+    
+    
+}
+
+- (IBAction)deleteKeyChain:(id)sender {
+    
+    NSError *error = nil;
+    
+    [FDKeychain deleteItemForKey: KEYCHAIN_KEY
+                      forService: KEYCHAIN_SERVICE
+                           error: &error];
+    
+    
+    NSLog(@"keychain deleted");
+    
+    
+}
+
+
 
 
 @end
